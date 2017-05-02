510c15e2d80a33fc3cb672c0e94a2f1721471221
olga3emes <olga3emes@gmail.com>
2017-05-03 01:45:36 +0200

    Modified Toolbar + bage of shopping-cart

--- app/src/main/java/com/example/olga/shop/MainActivity.java
+++ app/src/main/java/com/example/olga/shop/MainActivity.java
@@ -18,6 +18,9 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
