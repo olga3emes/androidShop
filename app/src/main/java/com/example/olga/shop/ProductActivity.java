8f9ebb9fe9e45f8a37d5a02fc15fa505c26b9eff
olga3emes <olga3emes@gmail.com>
2017-05-12 12:22:02 +0200

    Products + Layouts Modifications

--- app/src/main/java/com/example/olga/shop/ProductActivity.java
+++ app/src/main/java/com/example/olga/shop/ProductActivity.java
@@ -1,8 +1,12 @@
 package com.example.olga.shop;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
--- app/src/main/java/com/example/olga/shop/constant/Constant.java
+++ app/src/main/java/com/example/olga/shop/constant/Constant.java
@@ -10,15 +10,15 @@ public final class Constant {
     public static final List<Integer> QUANTITY_LIST = new ArrayList<Integer>();
 
     static {
-        for (int i = 1; i < 11; i++) QUANTITY_LIST.add(i);
+        for (int i = 1; i < 20; i++) QUANTITY_LIST.add(i);
     }
 
-    public static final Product PRODUCT1 = new Product(1, "Samsung Galaxy S6", BigDecimal.valueOf(199.996),
-            "Worldly looks and top-notch specs make the impressive, metal Samsung Galaxy S6 the Android phone to beat for 2015", "samsung_galaxy_s6");
-    public static final Product PRODUCT2 = new Product(2, "HTC One M8", BigDecimal.valueOf(449.9947),
-            "Excellent overall phone. Beautifull, battery life more than 20 hours daily and great customization in any way. 100% configuration on any aspect", "htc_one_m8");
-    public static final Product PRODUCT3 = new Product(3, "Xiaomi Mi3", BigDecimal.valueOf(319.998140),
-            "Xiaomi's Mi 3 is a showcase of how Chinese phonemakers can create quality hardware without breaking the bank. If you don't need 4G LTE, and you can get hold of it, this is one of the best smartphones you can buy in its price range.", "xiaomi_mi3");
+    public static final Product PRODUCT1 = new Product(1, "Pendientes Fantasía \n Color: Celeste", BigDecimal.valueOf(19.99),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", "pendientes");
+    public static final Product PRODUCT2 = new Product(2, "Mantoncillo Clásico \n Color: Verde_Pera", BigDecimal.valueOf(36.97),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", "mantoncillo");
+    public static final Product PRODUCT3 = new Product(3, "Peineta Borlas \n Color: Rojo", BigDecimal.valueOf(24.99),
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", "peineta");
 
     public static final List<Product> PRODUCT_LIST = new ArrayList<Product>();
 
